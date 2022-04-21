791e130ef03236d333d2a4a882e3ba7642f097a6
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-04-21 16:54:52 +0200

    Correct linter errors

--- solver.rb
+++ solver.rb
@@ -1,27 +1,27 @@
 class Solver
-    def find_factorial(arg)
-        if arg == 0
-            1
-        elsif arg < 0
-            "can't work on negative integers"
-        else
-            arg.downto(1).reduce(:*)
-        end
+  def find_factorial(arg)
+    if arg.zero?
+      1
+    elsif arg.negative?
+      "can't work on negative integers"
+    else
+      arg.downto(1).reduce(:*)
     end
-    
-    def reverse_string(arg)
-        arg.reverse
-    end
+  end
 
-    def fizzbuzz(arg)
-        if (arg % 3 == 0 && arg % 5 == 0)
-            "fizzbuzz"
-        elsif arg % 5 == 0
-            "buzz"
-        elsif arg % 3 == 0
-            "fizz"
-        else
-            arg.to_s
-        end
+  def reverse_string(arg)
+    arg.reverse
+  end
+
+  def fizzbuzz(arg)
+    if (arg % 3).zero? && (arg % 5).zero?
+      'fizzbuzz'
+    elsif (arg % 5).zero?
+      'buzz'
+    elsif (arg % 3).zero?
+      'fizz'
+    else
+      arg.to_s
     end
+  end
 end
