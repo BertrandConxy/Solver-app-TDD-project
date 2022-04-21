f734bb4c1e7961ad26c004acd779ec065cd7ea9b
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-04-21 16:34:36 +0200

    write tests for fizzbuzz method

--- Rspec_tests/solver_spec.rb
+++ Rspec_tests/solver_spec.rb
@@ -24,4 +24,22 @@ describe Solver do
             expect(@solver.reverse_string("hello")).to eql("olleh")
         end
     end
+
+    context "when given an integer argument" do
+        it "returns 'fizz' when argument is divisible by 3" do
+            expect(@solver.fizzbuzz(9)).to eql("fizz")
+        end
+        
+        it "returns 'buzz' when argument is divisible by 5" do
+            expect(@solver.fizzbuzz(10)).to eql("buzz")
+        end
+
+        it "returns 'fizzbuzz' when argument is divisible by 3 and 5" do
+            expect(@solver.fizzbuzz(15)).to eql("fizzbuzz")
+        end
+
+        it "returns that argument when it is not divisible by 3 or 5" do
+            expect(@solver.fizzbuzz(7)).to eql("7")
+        end
+    end
 end
